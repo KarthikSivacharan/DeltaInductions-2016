5ad3899f31c731f80ba14608901153520f55e1eb
Karthik Sivacharan <karthicksivacharan@gmail.com>
2016-06-17 23:06:59 +0800

    OOP , Sound , Gravity Implement

--- Task-2/Dinogame/jscript.js
+++ Task-2/Dinogame/jscript.js
@@ -1,22 +1,37 @@
-        xrect=565;
-        yrect= 225;
+var obstacle = {
+    
+    x:"565",
+    y:"225"
+    
+} ;
+
+var dino = {
+    
+    x:"100" ,
+    y:"245"
+    
+};
+
+
+        obstacle.x=565;
+        obstacle.y= 225;
         
         function animaterect() {
             
             
                 var canvas = document.getElementById("obstacle");
                 var context = canvas.getContext('2d');
-                context.clearRect(xrect+1,yrect,30,70);
+                context.clearRect(obstacle.x+1,obstacle.y,30,70);
                 context.beginPath();
-                context.rect(xrect,yrect,30,70);
+                context.rect(obstacle.x,obstacle.y,30,70);
                 context.fillStyle = 'yellow';
                 context.fill();
 
 
-                    if(xrect!= -30)
-                        xrect-=1;
+                    if(obstacle.x!= -30)
+                        obstacle.x-=1;
                     else
-                        xrect=565;
+                        obstacle.x=565;
             
             
        }
@@ -25,12 +40,12 @@
         
         function createplayer() {
             
-                xplayer = 100;
-                yplayer = 245;
+                dino.x = 100;
+                dino.y = 245;
                 var player = document.getElementById("obstacle");
                 ctext = player.getContext('2d');
                 ctext.beginPath();
-                ctext.rect(xplayer,yplayer,50,50);  
+                ctext.rect(dino.x,dino.y,50,50);  
                 ctext.fillStyle = 'black';
                 ctext.fill();
         
@@ -42,32 +57,34 @@
             
        
             
-            if(yplayer>=0) 
+            if(dino.y>=0) 
 
                 {
-                    ctext.clearRect(xplayer,yplayer+2,50,50);    
+                    ctext.clearRect(dino.x,dino.y+2,50,50);    
                     player = document.getElementById("obstacle");
                     ctext = player.getContext('2d');
                     ctext.beginPath();
-                    ctext.rect(xplayer,yplayer,50,50);  
+                    ctext.rect(dino.x,dino.y,50,50);  
                     ctext.fillStyle = 'black';
                     ctext.fill();  
-
-                   yplayer-=2;
-                   temp = yplayer; // storing yplayer temporarily for manipulation
+                    gravity = 1;
+                   dino.y = dino.y - 2 + gravity;
+                   temp = dino.y; // storing dino.y temporarily for manipulation
+                    gravity = gravity + 0.015;
 
                 }
 
             else
 
                 {     
-                    if(temp!=245)
+                    if(temp<=245)
 
-                    {
-                        temp+=2;
-                        ctext.clearRect(xplayer,temp-2,50,50);
+                    {   gravity = 0.05;
+                        temp = temp + 2 + gravity ;
+                        gravity = gravity + 0.015;
+                        ctext.clearRect(dino.x,temp-3,50,50);
                         ctext.beginPath();
-                        ctext.rect(xplayer,temp,50,50);
+                        ctext.rect(dino.x,temp,50,50);
                         ctext.fillStyle = 'black';
                         ctext.fill();
 
@@ -91,7 +108,8 @@
         
         function startgame() {
                         
-            
+             var startsound = new Audio("beep-07.mp3");
+             startsound.play();
              startplayer = setInterval(runplayer,1);  
              c= setInterval(count,100);// start the animmation of player 
              
@@ -105,31 +123,39 @@
         
         function count() {
             
+                
             
             
-            if(((xrect-xplayer==50 && xrect-xplayer>0) && (yrect-yplayer<=20 ) ) || ( (xrect-xplayer<50 && xrect-xplayer >=0 ) && yrect-yplayer<=50 ) || (xplayer-xrect<=30 && xplayer-xrect > 0) && yrect-yplayer==50 )
+            if(((obstacle.x-dino.x==50 && obstacle.x-dino.x>0) && (obstacle.y-dino.y<=20 ) ) || ( (obstacle.x-dino.x<50 && obstacle.x-dino.x >=0 ) && obstacle.y-dino.y<=50 ) || (dino.x-obstacle.x<=30 && dino.x-obstacle.x > 0) && obstacle.y-dino.y==50 )
             
             {
                
+              
+                
                 var c = document.getElementById("obstacle");
                 var ctx = c.getContext('2d');
                 ctx.font = "30px Century Gothic";
                 ctx.fillText("Game Over ! ", 210,50);
+                              
                 
                 clearInterval(start);
                 clearInterval(startplayer);
-                
-                
+                 
                             
             }
             
             else 
                 
-                score++;
+               { score++;
                 document.getElementById("score").innerHTML = score;
+               }
+            
+            
+                    
+            
         }
+
         
        
-        
-        
+       
       
